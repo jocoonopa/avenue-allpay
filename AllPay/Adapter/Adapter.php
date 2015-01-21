240d9cbaa787cb3a857b77e7e248fd714bb98450
jocoonopa <jocoonopa@gmail.com>
2015-01-21 18:22:15 +0800

    first commit

---
+++ AllPay/Adapter/Adapter.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Avenue\AllPay\Apdater;
+
+use Avenue\AllPay\Apdater\ITarget;
+
+class Adapter implements ITarget
+{
+    /**
+     * 初始化 AllPayInOne 元件
+     *
+     * @param string $hashKey [all in one 介接的 HashKey]
+     * @param string $hashIv [all in one 介接的 HashIV]
+     * @param string $merchantID [特店編號]
+     * 
+     * @return Avenue\AllPay\AllInOne
+     */
+    public function init($hashKey, $hashIv, $merchantID);
+
+    /**
+     * 結帳
+     *
+     * @param array $billInfo [訂單資訊關聯陣列]
+     */
+    public function pay();
+
+    /**
+     * 退款
+     */
+    public function cancel();
+
+    /**
+     * 通知結果
+     */
+    public function notify();
+
+    /**
+     * 查詢訂單
+     * @param  Avenue\AllPay\Strategy\Search $strategy
+     * @return json 
+     */
+    public function search(Search $strategy);
+}
